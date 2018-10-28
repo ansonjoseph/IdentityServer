b7bd1d7f99c0ece15025e369e559d295239943ab
ansonjoseph <ansonjoseph@gmail.com>
2018-10-28 12:59:49 -0400

    test submit

--- WenMax.Security.AuthServer/Startup.cs
+++ WenMax.Security.AuthServer/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace WenMax.Security.AuthServer
 {
+    //updated
     public class Startup
     {
         public Startup(IConfiguration configuration)
