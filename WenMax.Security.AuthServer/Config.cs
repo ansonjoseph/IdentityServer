5effb578e099f83b88f59401014e7496a5764861
ansonjoseph <ansonjoseph@gmail.com>
2018-10-28 12:55:47 -0400

    Add project files.

---
+++ WenMax.Security.AuthServer/Config.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityServer4.Models;
+namespace WenMax.Security.AuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client> {
+                new Client {
+                    ClientId ="ajoseph",
+                    AllowedGrantTypes =GrantTypes.ClientCredentials,
+                    ClientSecrets = {new Secret("password123".Sha256()) },
+                    AllowedScopes = { "api1","api2" }
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource> { new ApiResource("api1","My API") };
+        }
+    }
+}
